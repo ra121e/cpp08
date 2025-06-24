fe61a9c68c540925836bf403d7ca856c5f801733
root <sushi.honda@gmail.com>
2025-06-24 12:55:39 +0800

    ex00) fix: function signature
      In previous version, assumed input type is vector container with uncertain T.
      But the base type is also uncertain, then should be T
      input might be int vector or char list or .....
    
      have to put typename in front of T::iterator, bucause this is a kind of the type "T::iterator"
      we expects the T will be container and the collowing iterarot means type, not value
      we want to use variable it as something::iterator type.

--- ex00/easyfine.hpp
+++ ex00/easyfine.hpp
@@ -6,16 +6,16 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/24 09:53:50 by athonda           #+#    #+#             */
-/*   Updated: 2025/06/24 10:05:12 by athonda          ###   ########.fr       */
+/*   Updated: 2025/06/24 12:55:07 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <vector>
 
 template <typename T>
-std::vector::iterator	easyfind(std::vector<T> input, int	num)
+typename T::iterator	easyfind(T &input, int	num)
 {
-	for (std::vector::iterator it = input.begin(), it != input.end(), ++it)
+	for (typename T::iterator it = input.begin(), it != input.end(), ++it)
 	{
 		if (*it == num)
 			return (it);

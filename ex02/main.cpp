43d68a0605c5650fc38e21a0e7e442cc23612918
root <sushi.honda@gmail.com>
2025-06-28 19:15:39 +0800

    ex02) test ok

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 17:38:50 by athonda           #+#    #+#             */
-/*   Updated: 2025/06/28 19:08:05 by athonda          ###   ########.fr       */
+/*   Updated: 2025/06/28 19:15:00 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,24 @@ int	main(void)
 	Banner("simple test");
 	std::stack<int>	a;
 	std::cout << "stack a is empty or not: " << a.empty() << std::endl;
+	std::cout << "now, push number 5 on top." << std::endl;
 	a.push(5);
 	std::cout << "stack a: " << a.top() << std::endl;
+	std::cout << "now, push number 42 and 24 on top." << std::endl;
+	a.push(42);
+	a.push(24);
+	std::cout << "size of a: " << a.size() << std::endl;
+	std::cout << "stack a is empty or not: " << a.empty() << std::endl;
+	std::cout << "stack a: " << a.top() << std::endl;
+	std::cout << "now, pop on top." << std::endl;
+	a.pop();
+	std::cout << "stack a: " << a.top() << std::endl;
+
+	std::stack<int>	b;
+	std::cout << "now, make stack b and copy from a." << std::endl;
+	b = a;
+	std::cout << "stack b: " << a.top() << std::endl;
+
 
 	}
 

72b8aad283bab4b94ddaa658194ca0e99f19c60c
root <sushi.honda@gmail.com>
2025-06-28 17:09:44 +0800

    ex01) tests: 10000 case

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,16 +6,27 @@
 /*   By: athonda <athonda@student.42singapore.sg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 10:51:40 by athonda           #+#    #+#             */
-/*   Updated: 2025/06/28 16:22:30 by athonda          ###   ########.fr       */
+/*   Updated: 2025/06/28 17:09:36 by athonda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <ctime>
+#include <vector>
 #include <iostream>
 #include "Span.hpp"
 
 void	Banner(std::string title)
 {
 	std::cout << "\n--- " << title << " ---" << std::endl;
+}
+
+int		randomInt(void)
+{
+	unsigned int	high = static_cast<unsigned int>(std::rand()) & 0xFFFF;
+	unsigned int	low = static_cast<unsigned int>(std::rand()) & 0xFFFF;
+	unsigned int	combined = (high << 16) | low;
+
+	return (static_cast<int>(combined));
 }
 
 int	main(void)
@@ -46,6 +57,16 @@ int	main(void)
 	std::cout << container.longestSpan() << std::endl;
 
 	Banner("10000 test");
+	Span<int>	c(10000);
 
+	std::srand(static_cast<unsigned int>(std::time(0)));
+	std::size_t const	COUNT = 10000;
+
+	for (std::size_t i = 0; i < COUNT; ++i)
+	{
+		c.addNumber(randomInt());
+	}
+	std::cout << c.shortestSpan() << std::endl;
+	std::cout << c.longestSpan() << std::endl;
 	return (0);
 }
